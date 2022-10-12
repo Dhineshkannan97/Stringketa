f9beb3a746a920239dfa1d19311fac8bd120b8d5
Dhineshkannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-10-12 03:27:57 -0400

    stage 1

---
+++ src/main/java/Stringcalc.java
@@ -0,0 +1,17 @@
+public class Stringcalc {
+     public static int add(String num){
+         var splitNum = splitNum(num);
+         return pars(splitNum);
+     }
+     private static String[] splitNum(String num){
+         var split = num.split(",");
+         return split;
+     }
+     private static int pars(String[] split){
+         int p = 0;
+         for (String s : split) {
+             p = Integer.parseInt(s) + p;
+         }
+         return p;
+     }
+}
---
+++ src/test/java/stringcalcTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class stringcalcTest {
+@Test
+    void addTest(){
+    assertEquals(3, Stringcalc.add("1,2"));
+}
+
+}

39ce8b1c9aeae345f4d5044bd50c9b577d5fe8e4
Dhineshkannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-10-13 12:39:25 +0530

    stage 1

--- src/main/java/Stringcalc.java
+++ src/main/java/Stringcalc.java
@@ -1,24 +1,25 @@
 public class Stringcalc {
-     public static int add(String num){
-if (num.isEmpty()){
-    return 0;
-}else {
-//       if (){
-//
-//       }
-    var splitNum = splitNum(num);
-    return pars(splitNum);
-}
-     }
-     private static String[] splitNum(String num){
-         var split = num.split(",");
-         return split;
-     }
-     private static int pars(String[] split){
-         int p = 0;
-         for (String s : split) {
-             p = Integer.parseInt(s) + p;
-         }
-         return p;
-     }
+    public static int add(String num) {
+        if (num.isEmpty()) {
+            return 0;
+        } else {
+            var splitNum = splitNum(num);
+            return pars(splitNum);
+        }
+    }
+
+    private static String[] splitNum(String num) {
+        return num.split(",");
+    }
+
+    private static int pars(String[] split) {
+        int p = 0;
+        for (String s : split) {
+            if (!Character.isDigit(s.charAt(0))) {
+                continue;
+            }
+            p = Integer.parseInt(s) + p;
+        }
+        return p;
+    }
 }
--- src/test/java/stringcalcTest.java
+++ src/test/java/stringcalcTest.java
@@ -3,18 +3,28 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class stringcalcTest {
-@Test
-    void addTest(){
-    assertEquals(3, Stringcalc.add("1,2"));
-}
-@Test
-    void testEmptyString(){
-    assertEquals(0,Stringcalc.add(""));
-}
-@Test
+    @Test
+    void addTest() {
+        assertEquals(3, Stringcalc.add("1,2"));
+    }
+
+    @Test
+    void testEmptyString() {
+        assertEquals(0, Stringcalc.add(""));
+    }
+
+    @Test
     void testSingleNum() {
-    assertEquals(3, Stringcalc.add("3"));
-}
-@Test
-    void test
+        assertEquals(3, Stringcalc.add("3"));
+    }
+
+    @Test
+    void testOnlyInt() {
+        assertEquals(3, Stringcalc.add("3,A"));
+    }
+
+    @Test
+    void testBothAlpha() {
+        assertEquals(0, Stringcalc.add("A,A"));
+    }
 }

4027c383f1d066fdc488d4284344e872150ed62b
Dhineshkannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-10-18 20:20:37 +0530

    stage 1

--- src/main/java/Stringcalc.java
+++ src/main/java/Stringcalc.java
@@ -1,8 +1,13 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+class InvalidInputExp extends Exception {
+    InvalidInputExp(String msg) {
+        super(msg);
+    }
+}
 
 public class Stringcalc {
-    public static long add(String num) {
+    public static long add(String num) throws InvalidInputExp {
         if (num.isEmpty()) {
             return 0;
         } else {
@@ -15,18 +20,22 @@ public class Stringcalc {
         return num.split(",|\n");
     }
 
-    private static long pars(String[] split) {
+    private static long pars(String[] split) throws InvalidInputExp {
         long p = 0;
         for (String s : split) {
             Pattern pattern = Pattern.compile(".*[a-zA-Z]+.*");
             Pattern sym = Pattern.compile("[0-9]+");
             Matcher matcher = pattern.matcher(s);
             Matcher mat = sym.matcher(s);
-            if (s.isEmpty() || s.contains(".") || s.contains("-") || matcher.matches() || !mat.matches()) {
+            if (s.isEmpty() || s.contains(".") || s.contains("-") || matcher.matches() ||!mat.matches()) {
                 continue;
             }
-            //p = Long.parseLong(s) + p;
-            p = Math.addExact(Long.parseLong(s), p);
+            p = Long.parseLong(s) + p;
+//            p = Math.addExact(Long.parseLong(s), p);
+        }
+        if (p >= 9223372036854775807L) {
+            InvalidInputExp myExp = new InvalidInputExp("wrong input");
+            throw myExp;
         }
         return p;
     }
--- src/test/java/StringcalcTest.java
+++ src/test/java/StringcalcTest.java
@@ -5,68 +5,72 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringcalcTest {
     @Test
-    void addTest() {
+    void addTest() throws InvalidInputExp {
         assertEquals(3, Stringcalc.add("1,2"));
     }
 
     @Test
-    void testEmptyString() {
+    void testEmptyString() throws InvalidInputExp {
         assertEquals(0, Stringcalc.add(""));
     }
 
     @Test
-    void testSingleNum() {
+    void testSingleNum() throws InvalidInputExp {
         assertEquals(3, Stringcalc.add("3"));
     }
 
     @Test
-    void testOnlyInt() {
+    void testOnlyInt() throws InvalidInputExp {
         assertEquals(3, Stringcalc.add("3,A"));
     }
+
     @Test
-    void testBothAlpha() {
+    void testBothAlpha() throws InvalidInputExp {
         assertEquals(0, Stringcalc.add("A,A"));
     }
+
     @Test
-    void testMixAlpha(){
+    void testMixAlpha() throws InvalidInputExp {
         assertEquals(1, Stringcalc.add("1,3A"));
     }
+
     @Test
-    void testDecimal(){
+    void testDecimal() throws InvalidInputExp {
         assertEquals(1, Stringcalc.add("1,1.5"));
     }
+
     @Test
-    void testMutiInt(){
+    void testMutiInt() throws InvalidInputExp {
         assertEquals(45, Stringcalc.add("45"));
     }
 
     @Test
-    void testMutiInts() {
+    void testMutiInts() throws InvalidInputExp {
         assertEquals(100, Stringcalc.add("50,50"));
     }
 
     @Test
-    void testAlphaWithInt() {
+    void testAlphaWithInt() throws InvalidInputExp {
         assertEquals(61, Stringcalc.add("TEST34,61"));
     }
 
     @Test
-    void testUnknownAmtOfNum() {
+    void testUnknownAmtOfNum() throws InvalidInputExp {
         assertEquals(2147483652L, Stringcalc.add("5,2147483647"));
     }
 
     @Test
-    void testNegativeNum() {
+    void testNegativeNum() throws InvalidInputExp {
         assertEquals(3, Stringcalc.add("3,-4"));
     }
 
     @Test
-    void testIntWithMixAlpha() {
+    void testIntWithMixAlpha() throws InvalidInputExp {
         assertEquals(5, Stringcalc.add("13A,5"));
     }
 
     @Test
-    void testSymbol() {
+    void testSymbol() throws InvalidInputExp {
         assertEquals(54, Stringcalc.add("54,$$"));
     }
 
@@ -99,13 +103,17 @@ public class StringcalcTest {
     }
 
     @Test
-    void line() {
+    void line() throws InvalidInputExp {
         assertEquals(3, Stringcalc.add("1\n,2"));
     }
 
     @Test
-    void line1() {
+    void line1() throws InvalidInputExp {
         assertEquals(3, Stringcalc.add("1,\n2"));
+    }
+    @Test
+    void testMyExc1() throws InvalidInputExp {
+        assertThrows(InvalidInputExp.class,()->Stringcalc.add("9223372036854775809,5"));
     }
 }
 
